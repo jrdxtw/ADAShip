fd0804e10147deb909069b8b721afafd173bada9
Jordan Walters <jordan.walters@ada.ac.uk>
2021-03-24 00:33:59 +0000

    Gamesetup.cpp

--- Gamesetup.cpp
+++ Gamesetup.cpp
@@ -1,30 +1,33 @@
 #include "Gamesetup.h"
 //Function calls for header file within a folder "headers" for file "gamesetup.h"
 
-using namespace std;
-
 void place_a_ship(Boats &player) {
   string ship_entered, coordinate, lowercase_ship;
   vector<int> formatted_coordinate;
   int return_code;
   char ship_direction;
 
-  cout << "\nYour ship board:\n";
+  std::cout << "\n Your ship board:\n" << endl;
   player.print_ship_board();
+  //Function prints your board
 
-  cout << "Your ships:\n";
+  std::cout << "Your ships:\n" << endl;
   player.print_boats();
+  //Function prints your ships on "Gameboard"
 
-  cout << "\nEnter ship name: ";
+  std::cout << "\n Please enter ship type to contine: " << endl;
   getline(cin, ship_entered);
+  //Function prints the option to enter your desired vehicle
 
-  cout << "Enter where you want the ship to start (e.g. F7): ";
+  std::cout << "Enter coordinates for ship placement to start (e.g. F7): " << endl;
   getline(cin, coordinate);
+  //Function prints message to suggest to the player to enter coordinates to start game
 
   formatted_coordinate = player.format_coordinate(coordinate);
 
-  cout << "Is the ship vertical(V) or horizontal(H): ";
+  std::cout << "Is the ship vertical(V) or horizontal(H)?: " << endl;
   ship_direction = cin.get();
+  //Function is returing question to player if the vehicle is vertical or horizontal on the Gameboard
 
   lowercase_ship = lowercase_word(ship_entered);
   cout << "\nPlacing ship...\n";
@@ -32,15 +35,18 @@ void place_a_ship(Boats &player) {
   return_code = player.place_boat(formatted_coordinate, ship_direction, lowercase_ship);
   
   if (return_code == 0){
-    cout << "\nThe " << ship_entered << " ship is placed!\n";
+    std::cout << "\n The " << ship_entered << " ship is placed!\n" << endl;
     player.print_ship_board();
+    //Function notifies player that your desired ship is placed
   }
   else {
-    cout << "\nInvalid config, ship has not been placed.\nPlease try again\n";
+    std::cout << "\n invalid coordinates, vehicle has not been placed.\n Please try again \n" <<endl;
+    //System notifices player that they need to try again placing thier ship 
   }
 
   if (player.all_ships_placed()) {
-    cout << "\nall ships placed\n";
+    std::cout << "\n all ships have been placed! \n" << endl;
+    //System prints messages telling player that "all ships have been placed!"
   };
 }
 
@@ -49,11 +55,12 @@ void place_remaining_ships(Boats &player)
   int return_code;
   return_code = player.auto_place_remaining_ships();
   if (return_code == 0) {
-    cout << "\nAll remaining ships are placed!\n";
+    std::cout << "\n All remaining ships are placed!\n" << endl;
     player.print_ship_board();
+    //System prints messages telling player that "All remaining ships have been placed!
   }
   else {
-    cout << "\nERROR: All ships have already been placed.\nPlease reset the board first.\n";
+    std::cout << "\n ERROR: All ships have already been placed.\n Please reset the board first.\n" << endl;
   }
 }
 
@@ -64,23 +71,23 @@ void place_all_ships(Boats &player)
 
   if (return_code == 0) 
   {
-    cout << "\nAll ships are placed!\n";
+    std::cout << "\n All ships are placed!\n" << endl;
     player.print_ship_board();
   }
   else {
-    cout << "\nERROR: Ships have already been placed!\nPlease reset the board first or choose 'Auto-place remaining ships'\n";
+    std::cout << "\nERROR: Ships have already been placed!\nPlease reset the board first or choose 'Auto-place remaining ships'\n" << endl;
   }
 }
 
 void reset_board(Boats &player)
 {
   char reset_confirm;
-  cout << "\nAre you sure you want to reset the whole board?(Y/N)\n";
+  std::cout << "\nAre you sure you want to reset the whole board?(Y/N)\n" << endl;
 
   if( ! (cin >> reset_confirm))
    { 
   //catching erroneous input
-    cout << "Invalid input\n";
+    std::cout << "Invalid input\n" << endl;
     cin.clear(); 
   //clearing the error from cin stream
     cin.ignore(10000, '\n');
@@ -90,23 +97,23 @@ void reset_board(Boats &player)
   if (tolower(reset_confirm) == 'Y')
    {
     player.reset_board();
-    cout << "Board reset \n";
+    std::cout << "Board reset \n" << endl;
   }
 }
 
 void set_up_comp(Boats &_comp) 
 {
   system("clear");
-  cout << "\nSETTING UP THE COMPUTERS BOARD:\n";
+  std::cout << "\n SETTING UP THE COMPUTERS BOARD:\n" << endl;
   _comp.auto_place_all_ships();
 
-  cout << "\nThe computers ship board:\n";
+  std::cout << "\nThe computers ship board:\n" << endl;
   _comp.print_ship_board();
 
-  cout << "The computers ships:\n";
+  std::cout << "The computers ships:\n" << endl;
   _comp.print_boats();
 
-  cout << "\nPress enter to end computer setup...";
+  std::cout << "\nPress enter to end computer setup..." << endl;
   char temp = cin.get();
   cin.clear();
 }
@@ -117,74 +124,79 @@ int set_up_player(Boats &_player)
   string are_your_sure;
 
   system("clear");
-  cout << "\nSET UP YOUR BOARD:\n";
+  std::cout << "\nSET UP YOUR BOARD:\n" << endl;
 
   while (menu_choice != 0) 
   {
 
-    cout << "\nFirst you need to set up your board.\n";
-    cout << "1. Place a ship\n2. Auto-place remaining ships\n3. Auto-place all ships\n4. Reset board\n5. Continue to game\nPlease select an option (or 0 to quit): ";
+  std::cout << "\nFirst you need to set up your board.\n" << endl;
+  std::cout << "1. Place a ship\n2. Auto-place remaining ships\n3. Auto-place all ships\n4. Reset board\n5. Continue to game\nPlease select an option (or 0 to quit): " << endl;
+  //System will provide player with the following options to continue and attack
   
-    if( ! (cin >> menu_choice)) 
-    { 
-    //catching erroneous input
-      cout << "Invalid input\n";
-      cin.clear(); 
-    //clearing the error from cin stream
-      cin.ignore(10000, '\n');
-      menu_choice = -1;
-      continue; 
-    //continuing to the next iteration of the loop
-    }
-    cin.ignore();
+  if( ! (cin >> menu_choice)) 
+  { 
+  //catching erroneous input
+    std::cout << "Invalid input\n" << endl;
+    cin.clear(); 
+  //clearing the error from cin stream
+    cin.ignore(10000, '\n');
+    menu_choice = -1;
+    continue; 
+  //continuing to the next iteration of the loop
+  }
+  cin.ignore();
 
-    switch (menu_choice) 
-    {
-      case 1:
-        place_a_ship(_player);
-        break;
+  switch (menu_choice) 
+  {
+    case 1:
+    place_a_ship(_player);
+    break;
 
-      case 2:
-        place_remaining_ships(_player);
-        break;
+    case 2:
+    place_remaining_ships(_player);
+    break;
 
-      case 3:
-        place_all_ships(_player);
-        break;
+    case 3:
+    place_all_ships(_player);
+    break;
       
-      case 4:
-        reset_board(_player);
-        break;
-      case 5:
-        if (_player.all_ships_placed()) 
-        {
+    case 4:
+    reset_board(_player);
+    break;
+    case 5:
+    
+    if (_player.all_ships_placed()) 
+    {
 
-          cout << "\nAre you sure you're happy with the current placement(Y/N): ";
-          getline(cin, are_your_sure);
-          are_your_sure = lowercase_word(are_your_sure);
+    std::cout << "\n Please confirm current coordinate placement is correct?(Y/N): " << endl;
+    getline(cin, are_your_sure);
+    are_your_sure = lowercase_word(are_your_sure);
 
-          if(are_your_sure == "y" || are_your_sure == "yes") 
-          {
-            cout << "\nPress enter to end your setup...";
-            char temp = cin.get();
-            cin.clear();
+    if(are_your_sure == "Y" || are_your_sure == "Yes") 
+    {
+      std::cout << "\n Press enter to end your setup..." << endl;
+      //System will indicate to plater to complete their go and initiate next turn for computer
+      char temp = cin.get();
+      cin.clear();
 
-            return 0;
-          }
-          else 
-          {
-            cout << "\nReturning to menu...\n";
-            break;
-          }
-        }
-        else 
-        {
-          cout << "\nPlease place all ships before continuing\n";
-          break;
-        }
-      default:
-        cout << "\nInvalid Input, try again...\n";
-        break;
+      return 0;
+    }
+    else 
+      {
+      std::cout << "\n Returning to Menu...\n" << endl;
+      //System will print message to player notifying them that the system will return to menu
+      break;
+      }
+    }
+    else 
+      {
+      std::cout << "\n Please place all ships before continuing \n" << endl;
+      //System will notify player to "Please place all ships before continuing"
+      break;
+      }
+    default:
+      std::cout << "\n Coordinate Input invald, please try again...\n" << endl;
+      break;
     }
   }
 
